58c64a3bd905fb4ecf0efc2e0c3184e76ae4b641
emmanuel.eguaks@gmail.com <emmanuel.eguaks@gmail.com>
2021-12-11 16:17:47 +0100

    Added Command Model

--- Controllers/CommandsController.cs
+++ Controllers/CommandsController.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace CommandApi.Controllers{
     [Route("api/commands")]
     [ApiController]
-    public class CommandsApi : ControllerBase{
+    public class Commands : ControllerBase{
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get(){
             return new string[] {"my", "life", "is", "sweet"};
